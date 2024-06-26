6b4b7428f84a406f5cd01ada7cb641cd9f0f54e1
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-06-26 09:58:44 +0200

    Backup all

--- backup_core_keeper.sh
+++ backup_core_keeper.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cp -r /home/richel/.config/unity3d/Pugstorm/'Core Keeper'/Steam/174343888/saves .
 cp -r /home/richel/.config/unity3d/Pugstorm/'Core Keeper'/Steam/174343888/maps .
+cp -r /home/richel/.config/unity3d/Pugstorm/'Core Keeper'/Steam/174343888/worlds .
+cp -r /home/richel/.config/unity3d/Pugstorm/'Core Keeper'/Steam/174343888/worldinfos .
